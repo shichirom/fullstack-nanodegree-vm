bdcaa4189f0b0703cae109e52cd8278968379279
shichirom <shichirom@gmail.com>
2016-10-11 20:50:22 -0700

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,18 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+create table tournament
+(
+    id      integer 0 AUTO_INCREMENT Primary Key
+    player  varchar(20)
+    win     integer 0
+    matche  integer 0
+);
 
-
+create view tournament_view
+(
+    id      integer Not Null
+    player  varchar(20)
+    wins    integer 0
+    matche  integer 0
+);
